42750d7a5c0891178a39695116757ce9b289eb22
Shamilka21 <shamilstayathome23@gmail.com>
2021-11-15 20:52:01 +0600

    shama

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -1,54 +1,26 @@
-<<<<<<< HEAD
-import { Grid } from '@material-ui/core';
-import React, { useState } from 'react';
-import ProductCard from './ProductCard';
-
-const ProductsList = ({ products }) => {
-  const cart = JSON.parse(localStorage.getItem('cart')) ?? false;
-  const [color, setColor] = useState('red');
-
-  const onClick = () => {
-    setColor('blue');
-  };
-
-  // const sortedUser = useMemo (() => {
-  //     return [...users].sort((a,b) => a.age - b.age)
-  // }, [users]) -- НА ВСЯКИЙ СЛУЧАЙ ДЛЯ ХУКА USE_MEMO - КОТОРЫЙ ЗАПОМИНАЕТ
-
-  return (
-    <Grid container spacing={3}>
-      {/* <Button onClick={onClick}>change color</Button>
-            <div>{color}</div> -- НА ВСЯКИЙ СЛУЧАЙ ДЛЯ ХУКА USE_MEMO - КОТОРЫЙ ЗАПОМИНАЕТ */}
-      {products.map((product) => (
-        <Grid item xs={4} key={product.id}>
-          <ProductCard product={product} cart={cart} />
-=======
 import { Button, Grid } from '@material-ui/core';
 import React, { useMemo } from 'react';
 import ProductCard from './ProductCard';
 
 const ProductsList = ({ products }) => {
-    const cart = JSON.parse(localStorage.getItem('cart')) ?? false;
-    // const [color, setColor] = useState('red');
+  const cart = JSON.parse(localStorage.getItem('cart')) ?? false;
+  // const [color, setColor] = useState('red');
 
-    // const onClick = () => {
-    //     setColor('blue');
-    // };
+  // const onClick = () => {
+  //     setColor('blue');
+  // };
 
-    // const sortedUser = useMemo(() => {
-    //     return [...users].sort((a, b) => a.age - b.age);
-    // }, [users]);
+  // const sortedUser = useMemo(() => {
+  //     return [...users].sort((a, b) => a.age - b.age);
+  // }, [users]);
 
-    return (
-        <Grid container spacing={3}>
-            {/* <Button onClick={onClick}>change color</Button>
+  return (
+    <Grid container spacing={3}>
+      {/* <Button onClick={onClick}>change color</Button>
             <div>{color}</div> */}
-            {products.map((product) => (
-                <Grid item xs={4} key={product.id}>
-                    <ProductCard product={product} cart={cart} />
-                </Grid>
-            ))}
->>>>>>> 4a06c82a1da6b0b7bb82cb62ca84aea0dba49711
+      {products.map((product) => (
+        <Grid item xs={4} key={product.id}>
+          <ProductCard product={product} cart={cart} />
         </Grid>
       ))}
     </Grid>
